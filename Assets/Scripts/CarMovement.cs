896ab376b3d8f4ea0104c8d9c75d68e5b8fea70e
Gauti <gudmundurgj20@ru.is>
2022-11-28 13:09:23 +0000

    added CarMovement

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    
+    public int speed;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
+        if (Input.GetButton("Horizontal"))
+        {
+            transform.Rotate(0, 0, rotationSpeed * Time.fixedDeltaTime);
+        }
+    }
+}

ae05b458bcdf30e0ebf2b2b5d3caeb2b53ce24e5
Eythor <eythorh20@ru.is>
2022-11-28 16:21:19 +0000

    Minor coal + coal pickup mechanic

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -7,19 +7,38 @@ public class CarMovement : MonoBehaviour
     
     private int carSpeed;
     public float rotationSpeed;
+    private int maxCoalAmount;
+    private int currentCoal;
     // Start is called before the first frame update
     void Start()
     {
-        carSpeed = GameManager.instance.carSpeed;
+        carSpeed = GameManager.instance.carSpeed; // Get the car's current speed from the Game Manager
+        maxCoalAmount = GameManager.instance.maxCoalAmount; // Get the starting amount of maximum coal from the Game Manager
+        RefillCoal();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.right * Time.deltaTime * carSpeed);
-        if (Input.GetButton("Horizontal"))
-        {
-            transform.Rotate(0, 0, rotationSpeed * Time.fixedDeltaTime);
+        if (currentCoal > 0) {
+            transform.Translate(Vector2.right * Time.deltaTime * carSpeed);
+            if (Input.GetButton("Horizontal"))
+            {
+                transform.Rotate(0, 0, rotationSpeed * Time.fixedDeltaTime);
+            }
+            int negCoal = (int) Time.fixedTime;
+            currentCoal -= negCoal;
+            // Debug.Log(negCoal);
+            // Debug.Log(currentCoal);
         }
+    }
+    public void SetCoal(int newCoalAmount) {
+        // Update the max amount of coal that the car can use
+        maxCoalAmount = newCoalAmount;
+    }
+
+    public void RefillCoal() {
+        // Refill the coal tank to full
+        currentCoal = maxCoalAmount;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,16 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public int carSpeed;
+    public int maxCoalAmount;
     // Start is called before the first frame update
     void Awake()
     {
         instance = this;
+    }
+
+    public void RefillCoal() {
+        CarMovement car = GameObject.Find("Car").GetComponent<CarMovement>();
+        car.RefillCoal();
+        Debug.Log("Car has been refilled!!!");
     }
 }

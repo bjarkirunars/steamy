b985935c1ef69a7ff04f15b0dde86fbe56278dcf
bjarkirunars <bjarkifreyrrunars@gmail.com>
2022-12-01 15:41:48 +0000

    Raise penalty

--- Assets/Scripts/SteamObstacle.cs
+++ Assets/Scripts/SteamObstacle.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class SteamObstacle : MonoBehaviour
 {
-    private int speedPenalty = 4;
+    private int speedPenalty = 250;
 
     private void OnTriggerEnter2D(Collider2D other) {
         // Decrease car speed when car enters the steam obstacle
         Debug.Log(other.gameObject.tag);
         if (other.gameObject.tag == "CarHitBox") {
             Debug.Log("Hello");
-            GameManager.instance.carSpeed -= 100;
+            GameManager.instance.carSpeed -= speedPenalty;
             // Debug.Log("Car detected" + GameManager.instance.carSpeed.ToString());
         }
     }
--- Assets/Scripts/junkController.cs
+++ Assets/Scripts/junkController.cs
@@ -6,23 +6,23 @@ using static Unity.Mathematics.math;
 public class junkController : MonoBehaviour
 {
 
-    private int penalityWeight = 4;
+    private int speedPenalty = 250;
 
  
     private void OnCollisionEnter2D(Collision2D collision) {
             // Decrease car speed when car is colliding with junk
 
             if (collision.gameObject.name == "Car") {
-                GameManager.instance.carSpeed -=  penalityWeight ; // ADD (+ 1*grip)
-                Debug.Log("Car detected" + GameManager.instance.carSpeed.ToString());
+                GameManager.instance.carSpeed -=  speedPenalty ; // ADD (+ 1*grip)
+                // Debug.Log("Car detected" + GameManager.instance.carSpeed.ToString());
                 StartCoroutine(destroyer(collision));
             }
         }
     private void OnCollisionExit2D(Collision2D collision){
         // Increase car speed when junk is gone
         if (collision.gameObject.name == "Car") {
-                GameManager.instance.carSpeed += penalityWeight;
-                Debug.Log("Car detected" + GameManager.instance.carSpeed.ToString());
+                GameManager.instance.carSpeed += speedPenalty;
+                // Debug.Log("Car detected" + GameManager.instance.carSpeed.ToString());
                 
             }
     }

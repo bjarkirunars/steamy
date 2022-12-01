f5d005c518f48e01912c763576d1b0b0cdcca011
Daníel Heiðar Tómasson <doktordanni@gmail.com>
2022-12-01 14:35:12 +0000

    LEVEL COMPLETE POG, junk is better as well

--- Assets/Scripts/junkController.cs
+++ Assets/Scripts/junkController.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.Mathematics.math;
 
 public class junkController : MonoBehaviour
 {
 
-    public int penalityWeight;
+    private int penalityWeight = 4;
 
  
     private void OnCollisionEnter2D(Collision2D collision) {
             // Decrease car speed when car is colliding with junk
 
             if (collision.gameObject.name == "Car") {
-                GameManager.instance.carSpeed -= penalityWeight;
+                GameManager.instance.carSpeed -=  penalityWeight ; // ADD (+ 1*grip)
                 Debug.Log("Car detected" + GameManager.instance.carSpeed.ToString());
                 StartCoroutine(destroyer(collision));
             }

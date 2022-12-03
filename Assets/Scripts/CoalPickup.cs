9881e6d22ca7dc95dc2ea584f7b78b76b7087bdf
RedIceGuy <eythor0156@gmail.com>
2022-12-03 21:04:25 +0000

    Junk can no longer collect coal pickups

--- Assets/Scripts/CoalPickup.cs
+++ Assets/Scripts/CoalPickup.cs
@@ -7,9 +7,11 @@ public class CoalPickup : MonoBehaviour
     public AudioClip coalPickupSound;
 
     private void OnTriggerEnter2D(Collider2D other) {
-        GameManager.instance.PlayClip(coalPickupSound);
-        if (GameManager.instance.gameRunning) GameManager.instance.RefillCoal();
-        // Prevents car from regaining fuel if Game Over has been achieved
-        Destroy(gameObject);
+        if (other.gameObject.tag == "CarHitBox") {
+            GameManager.instance.PlayClip(coalPickupSound);
+            if (GameManager.instance.gameRunning) GameManager.instance.RefillCoal();
+            // Prevents car from regaining fuel if Game Over has been achieved
+            Destroy(gameObject);
+        }
     }
 }

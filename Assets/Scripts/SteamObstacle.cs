142bcccf73fda339f9fe4feae3ed354e61234d57
RedIceGuy <eythor0156@gmail.com>
2022-12-05 16:09:49 +0000

    Minor steam obstacle debugging

--- Assets/Scripts/SteamObstacle.cs
+++ Assets/Scripts/SteamObstacle.cs
@@ -5,20 +5,18 @@ using UnityEngine;
 public class SteamObstacle : MonoBehaviour
 {
     private int speedPenalty = 500;
-    public LayerMask playerLayer;
 
     private void OnTriggerEnter2D(Collider2D other) {
         // Decrease car speed when car enters the steam obstacle
-        if (other.gameObject.layer == playerLayer) {
+        if (other.gameObject.name == "Car") {
             GameManager.instance.carSpeed -= speedPenalty;
-            // Debug.Log("Car detected" + GameManager.instance.carSpeed.ToString());
         }
     }
+
     private void OnTriggerExit2D(Collider2D other) {
         // Increase the car speed when the car exits the steam obstacle
-        if (other.gameObject.layer == playerLayer) {
+        if (other.gameObject.name == "Car") {
             GameManager.instance.carSpeed += speedPenalty;
-            // Debug.Log("Car detected" + GameManager.instance.carSpeed.ToString());
         }    
     }
 }

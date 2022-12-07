706a40eb90f6b64437fd50df688b368b88898c71
Sveittur <doktordanni@gmail.com>
2022-12-07 09:21:43 +0000

    coin pickup & pile of junk

--- Assets/Scripts/powerUpController.cs
+++ Assets/Scripts/powerUpController.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class srcewController : MonoBehaviour
+public class powerUpController : MonoBehaviour
 {
     public AudioClip screwPickupSound;
 
     private void OnCollisionEnter2D(Collision2D collision) {
         // Debug.Log("Car detected" + GameManager.instance.carSpeed.ToString());
 
-        if (collision.gameObject.name == "screw") {
+        if (collision.gameObject.layer == 13) {
             GameManager.instance.PlayClip(screwPickupSound);
             GameManager.instance.currency +=  10 ;
             Destroy(gameObject);
